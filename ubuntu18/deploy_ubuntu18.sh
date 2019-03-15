5744730be0f4226eaa06df59d4e8b03430315918
chenyu <you@example.com>
2019-03-15 15:00:38 +0800

    add ubuntu18 scripts

---
+++ ubuntu18/deploy_ubuntu18.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd `dirname $0`
+
+echo " >>> 备份默认源： /etc/apt/sources.list.bak"
+sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
+echo " >>> 设置清华源 "
+sudo mv ${PWD}/sources.list /etc/apt/
+
+echo " >>> 安装软件: vim, git, net-tools, terminator"
+sudo apt-get install -y vim git net-tools terminator
+
+echo " >>> 配置终端分屏工具"
+mkdir -p ~/.config/terminator/
+mv ${PWD}/terminator_config ~/.config/terminator/config
+
+echo " >>> 配置结束"
+
